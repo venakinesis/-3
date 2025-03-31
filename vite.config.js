971fbe5fa4ec0d5129432d97f5b81037cbe5ef12
venakinn <sugiyamavenera@gmail.com>
2025-04-01 01:17:55 +0500

    initiz

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/-3'
+  base: '/-3/', // Должно точно совпадать с названием репозитория
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+    rollupOptions: {
+      output: {
+        assetFileNames: 'assets/[name]-[hash][extname]',
+        entryFileNames: 'assets/[name]-[hash].js'
+      }
+    }
+  }
 })
